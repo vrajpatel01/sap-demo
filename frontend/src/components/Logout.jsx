58ba640b02c2eb26c3f7462d54322e4dd150eaea
Vraj <84759467+vrajpatel01@users.noreply.github.com>
2023-10-26 22:03:53 +0530

    project closed.

--- frontend/src/components/Logout.jsx
+++ frontend/src/components/Logout.jsx
@@ -3,9 +3,19 @@ import { useSignOut } from 'react-auth-kit'
 import { IoMdLogOut } from 'react-icons/io'
 
 const Logout = () => {
-    const signOut = useSignOut();
+    function deleteAllCookies() {
+        const cookies = document.cookie.split(";");
+
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i];
+            const eqPos = cookie.indexOf("=");
+            const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+            document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        }
+        window.location.replace("/login");
+    }
     return (
-        <div onClick={() => signOut()} className='absolute top-10 right-10 text-3xl p-2 rounded-full bg-red-400 text-white cursor-pointer'><IoMdLogOut /></div>
+        <div onClick={() => deleteAllCookies()} className='absolute top-10 right-10 text-3xl p-2 rounded-full bg-red-400 text-white cursor-pointer'><IoMdLogOut /></div>
     )
 }
 
